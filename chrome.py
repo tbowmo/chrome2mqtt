dde621d0a149384d9f5a917d6c6cb04b85d916c4
Thomas Bowman Mørch <thomas+github@bowmo.dk>
2019-08-04 11:24:19 +0200

    Adding default to cors_host

--- chrome.py
+++ chrome.py
@@ -67,11 +67,13 @@ api.casters = {
 class EnableCors(object):
     name = 'enable_cors'
     api = 2
-
+    cors_host = environ.get('CORS_HOST')
+    if not cors_host:
+        cors_host = '*'
     def apply(self, fn, context):
         def _enable_cors(*args, **kwargs):
             # set CORS headers
-            response.headers['Access-Control-Allow-Origin'] = environ['CORS_HOST']
+            response.headers['Access-Control-Allow-Origin'] = cors_host
             response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
             response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
