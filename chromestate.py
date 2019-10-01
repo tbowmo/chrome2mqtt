4449f308945792b77b760f0585cb3e0700b70320
Thomas Bowman Mørch <thomas+github@bowmo.dk>
2019-10-01 18:17:47 +0200

    Rewrite chromestate

--- chromestate.py
+++ chromestate.py
@@ -3,124 +3,129 @@
 """
 import json
 import logging
+from pychromecast.socket_client import CastStatus 
+from pychromecast.controllers.media import MediaStatus 
 
 class ChromeState:
-    """ Holds state of the chromecast player """
+    """ Holds state of the chromecast mediaStatus """
     __device_type = ""
     __chrome_app = ""
-    title = ""
-    artist = ""
-    album = ""
-    content = ""
-    id = ""
-    skip_fwd = False
-    skip_bck = False
-    pause = False
-    player_state = ""
-    volume = False
-    volume_level = 0
+    __title = ""
+    __artist = ""
+    __album = ""
+    __skip_fwd = False
+    __skip_bck = False
+    __pause = False
+    __player_state = ""
+    __volume = False
+    __volume_level = 0
+    __volume_level1 = 0
+
+    @property
+    def app(self):
+        return self.__chrome_app
+    
+    @property
+    def player_state(self):
+        return self.__player_state
+    
+    @property
+    def volume_level(self):
+        return self.__volume_level
+
+    @property
+    def volume_level1(self):
+        return self.__volume_level1
 
     def __init__(self, device):
+        print(device)
         if device.cast_type == 'cast':
             self.__device_type = 'video'
         else:
             self.__device_type = device.cast_type
         self.log = logging.getLogger('chromestate_' + device.cast_type)
 
-    def json_media(self):
+    @property
+    def media(self):
         media_dict = {
-            "title": self.title,
-            "artist":self.artist,
-            "album":self.album,
+            "title": self.__title,
+            "artist":self.__artist,
+            "album":self.__album,
         }
         return json.dumps(media_dict).encode('utf-8')
 
-    def json_state(self):
+    @property
+    def state(self):
         state_dict = {
-            "skip_fwd": self.skip_fwd,
-            "skip_bck": self.skip_bck,
-            "pause": self.pause,
-            "player_state": self.player_state,
-            "volume": self.volume,
-            "volume_level": self.volume_level,
+            "skip_fwd": self.__skip_fwd,
+            "skip_bck": self.__skip_bck,
+            "pause": self.__pause,
+            "player_state": self.__player_state,
+            "volume": self.__volume,
+            "volume_level": self.__volume_level,
+            "volume_level1": self.__volume_level1,
             "chrome_app": self.__chrome_app
         }
         return json.dumps(state_dict).encode('utf-8')
 
-    def __dtodict_(self):
-        """ Returns a json interpretation of the object """
-        return {
-            "title": self.title,
-            "artist":self.artist,
-            "album":self.album,
-            "content": self.content,
-            "skip_fwd": self.skip_fwd,
-            "skip_bck": self.skip_bck,
-            "pause": self.pause,
-            "id": self.id,
-            "player_state": self.player_state,
-            "volume": self.volume,
-            "volume_level": self.volume_level,
-            "chrome_app": self.__chrome_app
-        }
-
     def clear(self):
         """ Clear all fields """
-        self.player_state = "STOPPED"
+        self.__player_state = "STOPPED"
         self.__chrome_app = ""
-        self.id = ""
-        self.title = ""
-        self.artist = ""
-        self.content = ""
-        self.album = ""
-        self.media = ""
-        self.pause = False
-        self.skip_fwd = False
-        self.skip_bck = False
-        self.volume = False
-        self.volume_level = 0
+        self.__title = ""
+        self.__artist = ""
+        self.__album = ""
+        self.__pause = False
+        self.__skip_fwd = False
+        self.__skip_bck = False
+        self.__volume = False
+        self.__volume_level = 0
+        self.__volume_level1 = 0
 
-    def setApp(self, appName):
-        self.__chrome_app = appName
 
-    def app(self):
-        return self.__chrome_app
+    def setState(self, status: CastStatus):
+        app_name = status.display_name
+        if app_name is None or app_name == "Backdrop" or app_name == "" :
+            self.clear()
+            app_name = "None"
+        self.__chrome_app = app_name
+        self.__volume_level1 = status.volume_level * 100
 
-    def update(self, player):
-        if hasattr(player, 'player_state') and player.player_state is not None:
-            self.player_state = player.player_state
+    def setMedia(self, mediaStatus: MediaStatus):
+        if hasattr(mediaStatus, 'player_state') and mediaStatus.player_state is not None:
+            self.__player_state = mediaStatus.player_state
 
-        if hasattr(player, 'volume_level'):
-            self.volume_level = round(player.volume_level * 100)
+        if hasattr(mediaStatus , 'volume_level'):
+            print('xxx')
+            print(mediaStatus.volume_level)
+            self.__volume_level = round(mediaStatus.volume_level * 100)
 
         ch = None
-        if hasattr(player, 'supports_pause'):
-            self.pause = player.supports_pause
+        if hasattr(mediaStatus, 'supports_pause'):
+            self.__pause = mediaStatus.supports_pause
         else:
-            self.pause = False
+            self.__pause = False
 
-        if hasattr(player, 'supports_skip_forward'):
-            self.skip_fwd = player.supports_skip_forward
+        if hasattr(mediaStatus, 'supports_skip_forward'):
+            self.__skip_fwd = mediaStatus.supports_skip_forward
         else:
-            self.skip_fwd = False
+            self.__skip_fwd = False
 
-        if hasattr(player, 'supports_skip_backward'):
-            self.skip_bck = player.supports_skip_backward
+        if hasattr(mediaStatus, 'supports_skip_backward'):
+            self.__skip_bck = mediaStatus.supports_skip_backward
         else:
-            self.skip_bck = False
+            self.__skip_bck = False
 
-        if hasattr(player, 'supports_stream_volume'):
-            self.volume = player.supports_stream_volume
+        if hasattr(mediaStatus, 'supports_stream_volume'):
+            self.__volume = mediaStatus.supports_stream_volume
         else:
-            self.volume = False
+            self.__volume = False
 
-        self.id = None
+        if hasattr(mediaStatus, 'title'):
+            self.__title = mediaStatus.title
 
-        if hasattr(player, 'title'):
-            self.title = player.title
+        if hasattr(mediaStatus, 'artist'):
+            self.__artist = mediaStatus.artist
 
-        if hasattr(player, 'artist'):
-            self.artist = player.artist
-
-        if hasattr(player, 'album_name'):
-            self.album = player.album_name
+        if hasattr(mediaStatus, 'album_name'):
+            self.__album = mediaStatus.album_name
